b64db5574cc50ee94acfa99bc0a1d1ab0d938afc
장준태 <91938434+jangjuntae@users.noreply.github.com>
2025-02-14 00:51:56 +0900

    들여쓰기 조금 안맞았어서.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -47,7 +47,8 @@ public class Application {
             for (int i = 0; i < 3; i++) {
                 if (arr.get(i).equals(numArr.get(i))) {
                     strike++;
-                } else if (arr.contains(numArr.get(i))) {
+                } 
+                else if (arr.contains(numArr.get(i))) {
                     ball++;
                 }
             }
@@ -55,22 +56,22 @@ public class Application {
             if(strike == 0 && ball == 0){
                 System.out.println("낫싱");
             }
-             else{
+            else{
                 System.out.println(ball + "볼, " + strike +"스트라이크");
             }
 
-             if(strike == 3){
-                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                 int re = sc.nextInt();
+            if(strike == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int re = sc.nextInt();
 
-                 if(re == 1){
-                     strike = 0;
-                 }
-                 else{
-                     System.out.println("게임 종료");
-                 }
-             }
+                if(re == 1){
+                    strike = 0;
+                }
+                else{
+                    System.out.println("게임 종료");
+                }
+            }
         }
     }
 }
