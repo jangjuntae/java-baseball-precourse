52f49b355358d510f6d4008f143e5e821bc30339
jangjuntae <juntae0629@naver.com>
2025-02-16 02:19:38 +0900

    사람이 숫자 입력하는 모델

---
+++ src/main/java/baseball/model/InputModel.java
@@ -0,0 +1,16 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputModel {
+
+    public static String InputModel(){
+        String num = Console.readLine();
+
+        if (num.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        return num;
+    }
+}
